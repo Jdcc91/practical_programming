b0103957d6c1613b4985adc18a07688163888d39
Jdcc91 <92959139+Jdcc91@users.noreply.github.com>
2025-07-14 10:35:59 -0500

    Add files via upload

---
+++ five_times.py
@@ -0,0 +1,17 @@
+print("My name is")
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+print('\n')
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+print('\n')
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i = i + 1
+print('\n')
